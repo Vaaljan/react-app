11ddb99e3edec828b61fa8919443ca3483257089
Heinrich <heinrichc@infoslips.com>
2017-12-07 16:53:12 +0200

    Introduced sub route with products

--- app/js/components/products.js
+++ app/js/components/products.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router } from 'react-router-dom'
 
 class Products extends Component {
     handleRedirect(){
-        browserHistory.push('/cars');
+        this.props.routeProps.history.push("/shop")
     }
     render(){
+        const products = this.props.productData;
+        const id = this.props.routeProps.match.params.id;
+        const item = products.filter(item => {
+            if(item.id == id) {
+                return item;
+            }
+        });
         return(
-        <div>Products</div>
+            <div>
+                <h1>{item[0].name}</h1>
+                <div className="row">
+                    <div className="col-sm-6 col-md-4">
+                        <div className="thumbnail">
+                            <img src={item[0].media} alt={item[0].name} />
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-4">
+                       <ul>
+                           <li><strong>Model</strong>: {item[0].model}</li>
+                           <li><strong>Make</strong>: {item[0].make}</li>
+                           <li><strong>Year</strong>: {item[0].year}</li>
+                           <li><strong>Price</strong>: {item[0].price}</li>
+                       </ul>
+                    </div>
+                    <div className="col-md-12">
+                    <button className="btn btn-default" onClick={this.handleRedirect.bind(this)}>Go to Cars</button>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- app/js/components/shop.js
+++ app/js/components/shop.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Shop extends Component {
+
     render(){
+        const products = this.props.productData;
+        const productList = products.map((item)=>{
+            return(
+                <NavLink to={"/shop/"+item.id} className="list-group-item" key={item.id}>
+                 {item.name}
+                </NavLink>
+            )
+        });
 
         return (
             <div>
                 <h1>Shop page</h1>
+                <div className="list-group">
+                    {productList}
+                </div>
             </div>
         );
     }
